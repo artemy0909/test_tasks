06b038313b1679760fa535929d864479fa961a28
Артемий Лысиков <artemy0909@gmail.com>
2020-06-21 16:19:00 +0700

    Minor changes

--- task_2.py
+++ task_2.py
@@ -1,24 +1,18 @@
 import re
 import os
 
+
 def uppercase_inside_lowercase(line: str):
-    upper_before = False
-    first = None
-    second = None
+    i = 0
     result = []
-    for ch in line:
-        if first and second:
-            result.append((first, second))
-            first = None
-            second = None
-        if ch.islower():
-            if upper_before:
-                second = ch
-            else:
-                first = ch
-            upper_before = False
-        elif ch.isupper() and first:
-            upper_before = True
+    while i < len(line) - 5:
+        if line[i].islower():
+            y = i + 1
+            while line[y].isupper():
+                y += 1
+                if line[y].islower():
+                    result.append((line[i], line[y]))
+        i += 1
     return result
 
 
@@ -28,10 +22,15 @@ def find_inside_uppercase(line: str):
     while i < len(line) - 5:
         if line[i].isupper() and line[i+1:i+3].islower() and line[i+3:i+6].isupper():
             result.append((line[i], line[i+5]))
-            i += 6
-        else:
-            i += 1
+        i += 1
     return result
+
+
+def list_to_str(tuple_list):
+    string = f"'{tuple_list[0][0]}{tuple_list[0][1]}'"
+    for e in tuple_list[1:]:
+        string += f", '{e[0]}{e[1]}'"
+    return string
 
 
 if __name__ == '__main__':
@@ -43,12 +42,12 @@ if __name__ == '__main__':
         quit(1)
 
     re_find = re.findall(r"([a-z])[A-Z]+([a-z])", LINE)
-    print(f"1. с помощью re \t{re_find}")
-    print(f"1. без помощи re \t{uppercase_inside_lowercase(LINE)}")
+    print(f"1. с помощью re \t{list_to_str(re_find)}")
+    print(f"1. без помощи re \t{list_to_str(uppercase_inside_lowercase(LINE))}")
 
     re_find = re.findall(r"([A-Z])[a-z]{2}[A-Z]{2}([A-Z])", LINE)
-    print(f"2. с помощью re \t{re_find}")
-    print(f"2. без помощи re \t{find_inside_uppercase(LINE)}")
+    print(f"2. с помощью re \t{list_to_str(re_find)}")
+    print(f"2. без помощи re \t{list_to_str(find_inside_uppercase(LINE))}")
 
     dir_name = "task_2_files"
     if not os.path.exists(f"{dir_name}/"):
